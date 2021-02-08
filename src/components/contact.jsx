013d85c8c2d0fecfd649e3a0e231ce13b2a9d5b7
ashrithyvs <ashrithy20@gmail.com>
2021-02-08 11:43:05 +0530

    fixed contact section issue

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-import { Button, Col } from "react-bootstrap";
+import { Button, Col, Modal } from "react-bootstrap";
 import emailjs from "emailjs-com";
 
-
 function Contact() {
+  const [show, setShow] = useState(false);
+  const [showError, setShowError] = useState(false);
   const [validated, setValidated] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleCloseError = () => setShowError(false);
+
   const handleSubmit = (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
+      setShowError(true);
+    } else {
+      sendEmail(event);
+      setValidated(true);
+      setShow(true);
     }
-    setValidated(true);
   };
 
-  
   function sendEmail(e) {
     e.preventDefault();
 
@@ -37,31 +45,54 @@ function Contact() {
     e.target.reset();
   }
 
-  
-
-  
-
   return (
     <div>
       <section
-        style={{ display: "grid", alignContent: "center",paddingBottom:"5%",paddingTop:"5%"}}
+        style={{
+          display: "grid",
+          alignContent: "center",
+          paddingBottom: "5%",
+          paddingTop: "5%",
+        }}
         id="contact"
       >
         <h1 class="text-center mb-5">Want to Ping me?</h1>
         <Form
           noValidate
           validated={validated}
-          onSubmit={sendEmail}
-          style={{ padding: "5% auto 5%",width:"40%", margin:"auto"}}
+          onSubmit={(event) => handleSubmit(event)}
+          style={{ padding: "5% auto 5%", width: "40%", margin: "auto" }}
         >
           <Form.Group>
             <Form.Label>Enter your Name</Form.Label>
-            <Form.Row>
+            <Form.Row className="d-flex justify-content-around">
               <Col>
-                <Form.Control required type="text" name="first_name" placeholder="First Name" />
+                <Form.Control
+                  required
+                  type="text"
+                  name="first_name"
+                  placeholder="First Name"
+                />
+                <Form.Control.Feedback className="text-light">
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback className="text-light" type="invalid">
+                  Enter your First name.
+                </Form.Control.Feedback>
               </Col>
               <Col>
-                <Form.Control required type="text" name="last_name" placeholder="Last Name" />
+                <Form.Control
+                  required
+                  type="text"
+                  name="last_name"
+                  placeholder="Last Name"
+                />
+                <Form.Control.Feedback className="text-light">
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback className="text-light" type="invalid">
+                  Enter your Last name.
+                </Form.Control.Feedback>
               </Col>
             </Form.Row>
           </Form.Group>
@@ -75,6 +106,9 @@ function Contact() {
                   type="email"
                   placeholder="Your Email Address"
                 />
+                <Form.Control.Feedback className="text-light" type="invalid">
+                  Enter your vaild Email ID.
+                </Form.Control.Feedback>
               </Col>
             </Form.Row>
           </Form.Group>
@@ -88,26 +122,65 @@ function Contact() {
                   type="tel"
                   placeholder="Your Phone Number"
                 />
+                <Form.Control.Feedback className="text-light" type="invalid">
+                  Enter your vaild Phone Number.
+                </Form.Control.Feedback>
               </Col>
             </Form.Row>
           </Form.Group>
           <Form.Group>
             <Form.Row>
               <Col>
-                <Form.Label>Message (optional)</Form.Label>
+                <Form.Label>Message</Form.Label>
                 <Form.Control
                   as="textarea"
                   name="message"
+                  required
                   rows={5}
                   placeholder="Enter your Message here"
                 />
               </Col>
+              <Form.Control.Feedback className="text-light" type="invalid">
+                Enter your Message.
+              </Form.Control.Feedback>
             </Form.Row>
           </Form.Group>
           <div class="d-flex justify-content-center mt-5">
-          <Button variant="outline-light" type="submit" value="Send">
-            Submit
-          </Button></div>
+            <Button variant="outline-light" type="submit" value="Send">
+              Submit
+            </Button>
+
+            <Modal show={show} onHide={handleClose} size="lg">
+              <Modal.Header className="bg-success" closeButton>
+                <Modal.Title>
+                  <h5 className="text-light">Confirmation alert!</h5>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h5>
+                  Your message is sent successfully! Kindly check the
+                  confirmation mail sent to you ;)
+                </h5>
+                <Button variant="success" size="sm" onClick={handleClose}>
+                  close
+                </Button>
+              </Modal.Body>
+            </Modal>
+            <Modal show={showError} onHide={handleCloseError} size="lg">
+              <Modal.Header className="bg-danger" closeButton>
+                <Modal.Title>
+                  <h5 className="text-light">Error!</h5>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h4>Please re-enter your details and try again.</h4>
+                <br />
+                <Button variant="danger" size="sm" onClick={handleCloseError}>
+                  close
+                </Button>
+              </Modal.Body>
+            </Modal>
+          </div>
         </Form>
       </section>
     </div>
